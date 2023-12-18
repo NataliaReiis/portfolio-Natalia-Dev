e3f72784f2db9538fa601460a585bed13fee6896
NataliaReiis <nataliareis208@gmail.com>
2023-12-18 01:48:09 -0300

    feat: home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,28 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-import Logo from '../assets/icon/WLogo.svg'
+import Logo from "../assets/icon/WLogo.svg";
 
- export default function Navbar(){
-    return(
-        <>
-        <header>
-            <img src={Logo} alt="" />
-             <nav>
-                <ul>
-                    <Link to="/"><li>Home</li></Link>
-                    <Link to="/profile"><li>Perfil</li></Link>
-                    <Link to="/projects"><li>Projects</li></Link>
-                    <Link to="/contact"><li>Contact</li></Link>
-                </ul>
-             </nav>
-             <button>Blog</button>
-        </header>
-           
-        </>
-    )
- }
+export default function Navbar() {
+  return (
+    <>
+      <header>
+        <img src={Logo} alt="Logo natalia reis" />
+          <ul>
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/profile">
+              <li>Perfil e Habilidades</li>
+            </Link>
+            <Link to="/projects">
+              <li>Projetos</li>
+            </Link>
+            <Link to="/contact">
+              <li>Contato</li>
+            </Link>
+          </ul>
+        <button className="button-blog" >Blog</button>
+      </header>
+    </>
+  );
+}
