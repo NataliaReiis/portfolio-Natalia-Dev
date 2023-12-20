820def66c6b97e84431f1e82c813cab1c25508d1
NataliaReiis <nataliareis208@gmail.com>
2023-12-20 00:42:49 -0300

    feat: TimeLine

---
+++ src/components/Skills.jsx
@@ -0,0 +1,44 @@
+import { PieChart } from "@mui/x-charts";
+
+import { SiHtml5, SiCss3, SiSass, SiJavascript, SiReact, SiNodedotjs, SiMysql, SiLinuxcontainers, SiFigma, SiAdobephotoshop } from "react-icons/si";
+
+
+export default function Skills() {
+  return (
+      <div className="skills">
+        <div className="chart">
+          <PieChart
+            series={[
+              {
+                data: [
+                  { id: 0, value: 45, label: "Front-end" },
+                  { id: 1, value: 35, label: "UI/UX Design" },
+                  { id: 2, value: 20, label: "Back-end" },
+                ],
+                innerRadius: 34,
+                paddingAngle: 10,
+                cornerRadius: 4,
+                endAngle: 400,
+              },
+            ]}
+            width={400}
+            height={200}
+          />
+
+          <div className="languages">
+            <button>HTML <SiHtml5/></button>
+            <button>CSS <SiCss3/></button>
+            <button>Sass <SiSass/></button>
+            <button>Javascript < SiJavascript/></button>
+            <button>ReactJs <SiReact/></button>
+            <button>NodeJs <SiNodedotjs/></button>
+            <button>MySql <SiMysql/></button>
+            <button>Linux <SiLinuxcontainers/></button>
+            <button>Figma <SiFigma/></button>
+            <button>Photoshop<SiAdobephotoshop/></button>
+          </div>
+        </div>
+        
+      </div>
+  );
+}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,79 @@
+import Navbar from "../components/Navbar";
+import Skills from '../components/Skills';
+import TimeLine from '../components/Line'
 
-export default function Profile(){
-    return(
-       <div className="container-global">
+import { FiCode } from "react-icons/fi";
+import { BsBrush } from "react-icons/bs";
 
-       </div>
-    )
+import ImageAbout from "../assets/image/about-image.png";
+import code from "../assets/icon/code.svg";
+import design from "../assets/icon/design.svg";
+
+export default function Profile() {
+  return (
+    <div className="profile">
+      <Navbar />
+      <div className="about-infos-container">
+        <div className="about-text">
+          <h1>Um pouco sobre mim...</h1>
+          <p>
+            Me chamo Natália, sou estudante de Ciência da Computação no 5º semestre. Estou super
+            empolgada com a jornada de transformar conceitos em experiências
+            digitais. Meu caminho começou no design e front-end, mas agora estou
+            mergulhando também na estrutura de dados e lógica de programação.
+            Tenho um olhar atento para construir sistemas eficientes, combinando
+            meu entendimento de estrutura de dados e lógica de programação com a
+            habilidade de criar interfaces intuitivas. Meu site é mais do que
+            linhas de código; é uma mistura equilibrada entre funcionalidade e
+            design.
+          </p>
+        </div>
+        <div className="image-about">
+          <img src={ImageAbout} alt="" />
+        </div>
+      </div>
+      <div className="code-design-container">
+        <div className="code-content">
+          <img src={code} alt="" />
+          <span className="design-code-text">
+            <h1>
+              Developer
+              <FiCode />
+            </h1>
+            <p>
+              Um lugar onde a estética encontra a funcionalidade. Como
+              desenvolvedor front-end, minha paixão é dar vida às visões dos
+              designers. Cada linha de código é uma oportunidade de criar uma
+              experiência interativa e responsiva que eleva a usabilidade e a
+              acessibilidade. Navegue pelos projetos e descubra como transformo
+              conceitos em interfaces dinâmicas e intuitivas.
+            </p>
+          </span>
+        </div>
+        <div className="design-content">
+          <span className="design-code-text">
+            <h1>
+              Design
+              <BsBrush />
+            </h1>
+            <p>
+              Meu portfólio é uma jornada visual através do mundo do design,
+              onde cada projeto conta uma história única. Sou um designer
+              apaixonado, dedicado a criar experiências envolventes e
+              esteticamente atraentes. Cada elemento do meu trabalho é
+              meticulosamente pensado, desde a paleta de cores até a tipografia
+              escolhida. Explore meu portfólio e descubra como transformo ideias
+              em designs que cativam e inspiram
+            </p>
+          </span>
+
+          <img src={design} alt="" />
+        </div>
+      </div> 
+      <h1 className="timeline-title">Jornada Acadêmica</h1>
+      <TimeLine />
+      <Skills />
+     
+    </div>
+  );
 }
