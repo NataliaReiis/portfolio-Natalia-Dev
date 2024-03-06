167530b4b672f0eea1d700e7e3b30fdcd9703798
NataliaReiis <nataliareis208@gmail.com>
2024-03-06 20:04:02 -0300

    feat: Gallery

--- src/components/project/Gallery.jsx
+++ src/components/project/Gallery.jsx
@@ -1,17 +1,25 @@
+import React from "react";
 import ImageGallery from "react-image-gallery";
+import 'react-image-gallery/styles/scss/image-gallery.scss';
 
-images const = [
+const images = [
     {
-        
-    }
-]
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+  ];
 
-export default function Gallery(){
-    return(
-        <>
-        <div className="container-galley">
-            <h1>Galeria de Arts</h1>
-        </div>
-        </>
-    )
-}
+  class Gallery extends React.Component {
+    render() {
+      return <ImageGallery className='gallery' items={images} />;
+    }
+  }
+  export default Gallery;
