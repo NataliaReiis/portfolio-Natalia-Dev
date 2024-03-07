c20f493b46d09ca5ba6012557f2f9934d7349231
NataliaReiis <nataliareis208@gmail.com>
2024-03-07 16:45:43 -0300

    feat: button redirect to back in component ProjectDetail

--- src/components/project/ProjectDetail.jsx
+++ src/components/project/ProjectDetail.jsx
@@ -1,12 +1,16 @@
 import { useParams } from "react-router-dom";
 
 export default function ProjectDetail() {
+
   const { id } = useParams();
 
+  const redirectToPage = () => {
+    window.history.back();
+  }
 
   return (
     <div>
-  
+      <button onClick={redirectToPage}>Voltar</button>
       <h1>Detalhes do projeto</h1>
       <p>Id do projeto: {id}</p>
     </div>
