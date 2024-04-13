835b05f1b763157be3b933523396a2fd3211ec97
NataliaReiis <nataliareis208@gmail.com>
2024-04-13 12:05:05 -0300

    feat: page about cocluded

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,17 +3,30 @@ import TimeLine from "../components/profile/TimeLine";
 import Navbar from "../components/Navbar"
 import About from  "../components/profile/About"
 
+import html from "../assets/image/html.png";
+import css from "../assets/image/css.png";
+import javascript from "../assets/image/Javascript.png";
+import sass from "../assets/image/Sass.png";
+import react from "../assets/image/react.png";
+import node from "../assets/image/Node.png"
+import sql from "../assets/image/SQL.png"
+import api from "../assets/image/API.png";
+import photoshop from "../assets/image/Photoshop.png";
+import figma from "../assets/image/Figma.png";
+import wordpress from "../assets/image/WordPress.png";
+
+
 export default function Profile() {
-  const othersServices = [
+  const otherServices = [
     {
-      name: 'Desenvolvimento',
+      name: '01. Desenvolvimento',
       item1: 'Web sites',
       item2: 'Webflows',
       item3: 'App Android',
       item4: 'Implantação ERP',
     },
     {
-      name: 'Design Visual',
+      name: '02. Design Visual',
       item1: 'Ui/Ux Design',
       item2: 'Design Graphic',
       item3: 'Icon Design',
@@ -29,23 +42,30 @@ export default function Profile() {
 
 
   <div className="others-services">
-      <div className="design-visual">
-        <h1>{othersServices.name}</h1>
-          <ul> {/* CORRIGIR ESSE TRECHO */}
-            <li>{othersServices.item1}</li>
-            <li>{othersServices.item2}</li>
-            <li>{othersServices.item3}</li>
-            <li>{othersServices.item4}</li>
-          </ul>
+  {otherServices.map((service, index) => (
+      <div key={index} className="container-services">
+        <h1>{service.name}</h1>
+           <ul> {/* CORRIGIR ESSE TRECHO */}
+            <li>{service.item1}</li>
+            <li>{service.item2}</li>
+            <li>{service.item3}</li>
+            <li>{service.item4}</li>
+          </ul> 
       </div>
-      <div className="development">
-        <h1></h1>
-          <ul>
-            <li></li>
-          </ul>
-      </div>
-
-
+      ))}
+  </div>
+  <div className="images-languages">
+      <img src={html} alt="" />
+      <img src={css} alt="" />
+      <img src={javascript} alt="" />
+      <img src={sass} alt="" />
+      <img src={react} alt="" />
+      <img src={node} alt="" />
+      <img src={sql} alt="" />
+      <img src={api} alt="" />
+      <img src={photoshop} alt="" />
+      <img src={figma} alt="" />
+      <img src={wordpress} alt="" />
   </div>
   </>
   );
