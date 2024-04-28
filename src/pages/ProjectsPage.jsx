2a73c61eeb6e1cc5b24f5d3bb0b916f74a4915f8
NataliaReiis <nataliareis208@gmail.com>
2024-04-28 18:15:22 -0300

    erro deploy test

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -2,7 +2,6 @@ import Pagination from '../components/project/Projects';
 import Gallery from '../components/project/Gallery';
 import Navbar from '../components/Navbar';
 
-import LogoDark from '../assets/image/LogoDark.png'
 
 export default function Prokects(){
     return(
